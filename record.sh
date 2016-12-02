2972e9f5c6b8ca9f8339fbe4a2f0b2a3e40ed9fe
geco <geckoemailaddress@gmail.com>
2016-12-02 15:58:18 +0100

    new record

--- record.sh
+++ record.sh
@@ -1,8 +1,4 @@
-ffmpeg \
-  -r 5 \
-  -thread_queue_size 512 \
-  -f v4l2 -framerate 25 -video_size 640x480 -i "/dev/video0" \
-  -thread_queue_size 512 \
-    -f alsa -ac 1 -channel_layout mono -ar 48000 -i "hw:CARD=Webcam,DEV=0" \
-  -c:v libvpx -crf 20  -ac 1 -c:a libvorbis \
-  record.webm
+ffmpeg -r 5 -thread_queue_size 512 -f v4l2 -video_size 640x480 \
+-i /dev/video0 -thread_queue_size 512 -f alsa -ac 1 -channel_layout mono -ar 48000 -i "hw:CARD=Webcam,DEV=0" \
+-c:v libvpx -crf 30  -ac 1 -c:a libvorbis -ab 32k \
+record.webm
